04a0fb81ac35735e41d18f8073654168688a1158
nofelmedia <77242923+nofelmedia@users.noreply.github.com>
2021-04-03 01:08:26 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,72 +1,29 @@
-let inputData = document.querySelector('#data');
-let inputselector = document.querySelector('#Selector');
-let inputname = document.querySelector('#name');
-let inputname2 = document.querySelector('#name2');
-let inputface = document.querySelector('#facebook_id');
-let inputfaceLink = document.querySelector('#Facejs');
-let inputfaceLink2 = document.querySelector('#Facejs2');
-let inputfaceLink3 = document.querySelector('#Facejs3');
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
-
-fetch('https://raw.githubusercontent.com/nofelmedia/BD_friend/main/bd%20friend.json')
-.then((response) => response.json())
-.then(data => {
-
-    let dataLenth = data.length;
-    for(i=0; i<dataLenth; i++){
-        let innerHtml = `
-   <option value="${i}"> ${data[i].login} </option>
-   `;
-   inputselector.innerHTML += innerHtml;
-    };
-   
-
-      console.log(data);
-
-})
-
-
-inputselector.addEventListener("change", function(){
-    let thisValue = this.value;
-    console.log(thisValue)
-    fetch('https://raw.githubusercontent.com/nofelmedia/BD_friend/main/bd%20friend.json')
-.then((response) => response.json())
-.then(data => {
-    // for main Image
-    let htmlData = `
-     <img src="${data[thisValue].avatar_url}"/>
-    `;
-    inputData.innerHTML = htmlData;
-
-    // for full name
-    // let htmlDat = `
-    //<span> ${data[thisValue].id_name}</span>
-    //`;
-    //inputname2.innerHTML = htmlDat;
-    
-    //for social name
-    let htmlDa = `
-    <h1> ${data[thisValue].id_name}</h1>
-    `;
-    inputname.innerHTML = htmlDa;
-
-    // social linl
-    let htmlfb = `
-   <a href="${data[thisValue].social_id}"> ${data[thisValue].id_name}</a>
-    `;
-    inputfaceLink.innerHTML = htmlfb;
-
-
-     // email
-    let htmlemail = `
-   <a > ${data[thisValue].email_name}</a>
-    `;
-    inputfaceLink2.innerHTML = htmlemail;
-
-    // phonw
-     let htmlphone = `
-   <a > ${data[thisValue].phone_number}</a>
-    `;
-    inputfaceLink3.innerHTML = htmlphone;
-})
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
 });
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+
+function myfunction(){
+			let x = document.getElementById('confirmbox2');
+			let y = document.getElementById('kiks1');
+			let z = document.getElementById('kiks2');
+
+			if(x.type === 'password'){
+				x.type = 'text';
+				y.style.display = 'none';
+				z.style.display = 'block';
+			}else{
+				x.type = 'password';
+				y.style.display = 'block';
+				z.style.display = 'none';
+			}
+		}
+  
